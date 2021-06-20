f7a75001501cb11285b1181eacd959351a7d112f
mm <laythirshaid1998@gmail.com>
2021-06-20 21:11:32 +0300

    Update and Delete MVC

---
+++ Update and Delete/MVC/src/main/java/com/MVC/repositories/BookRepository.java
@@ -0,0 +1,22 @@
+package com.MVC.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.MVC.models.Book;
+
+@Repository
+public interface BookRepository extends CrudRepository<Book, Long> {
+	List<Book> findAll();
+
+
+	List<Book> findByDescriptionContaining(String search);
+
+
+	Long countByTitleContaining(String search);
+
+
+	Long deleteByTitleStartingWith(String search);
+}
---
+++ Update and Delete/MVC/src/main/java/com/MVC/services/BookServices.java
@@ -0,0 +1,52 @@
+package com.MVC.services;
+
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.MVC.models.Book;
+import com.MVC.repositories.BookRepository;
+@Service
+public class BookServices {
+    private final BookRepository bookRepository;
+    
+    public BookServices(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> allBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book createBook(Book b) {
+        return bookRepository.save(b);
+    }
+ 
+    public Book findBook(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if(optionalBook.isPresent()) {
+            return optionalBook.get();
+        } else {
+            return null;
+        }
+    }
+
+	public Book updateBook(Long id, String title, String desc, String lang, Integer numOfPages) {
+		Book book1=this.findBook(id);
+		book1.setDescription(desc);
+		book1.setLanguage(lang);
+		book1.setTitle(title);
+		book1.setNumberOfPages(numOfPages);
+		
+		return bookRepository.save(book1);
+	}
+
+	public void deleteBook(Long id) {
+		bookRepository.deleteById(id);
+		
+	}
+	
+}
