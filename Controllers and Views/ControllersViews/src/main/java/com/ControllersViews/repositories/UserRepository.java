d33ef4bb328e822d951c45ea8403fa8a3561bd9a
mm <laythirshaid1998@gmail.com>
2021-06-28 00:01:28 +0300

    Controllers and Views

---
+++ Controllers and Views/ControllersViews/src/main/java/com/ControllersViews/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.ControllersViews.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ControllersViews.models.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByEmail(String email);
+}
---
+++ Controllers and Views/ControllersViews/src/test/java/com/ControllersViews/ControllersViewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ControllersViews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllersViewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
